952331412ddf76325fb8172fa4f7350ff9c61fbd
chuma-beep <wisboynelson123@gmail.com>
2026-05-08 13:32:31 +0100

    added shadcn to make tool tip betttter

--- viz/vite.config.ts
+++ viz/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    tsPaths(),
-  ],
+  plugins: [react(), tailwindcss(), tsPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
